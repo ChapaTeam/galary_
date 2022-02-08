4f5baeef70c8977d37e1a0dca138a85d99d1b57a
Yulia2120 <ket-dz@rambler.ru>
2022-02-08 09:17:26 +0200

    commit

--- galary/Form1.Designer.cs
+++ galary/Form1.Designer.cs
@@ -32,7 +32,7 @@ namespace galary
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Galary";
         }
 
         #endregion
