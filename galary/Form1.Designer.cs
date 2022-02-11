8528ab71f41a78c979b2566cf851c1e4bc46cd21
Yulia2120 <ket-dz@rambler.ru>
2022-02-11 14:59:29 +0200

    commit

--- galary/Form1.Designer.cs
+++ galary/Form1.Designer.cs
@@ -29,13 +29,135 @@ namespace galary
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.selectedImage = new System.Windows.Forms.PictureBox();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectedImage
+            // 
+            this.selectedImage.Location = new System.Drawing.Point(104, 43);
+            this.selectedImage.Name = "selectedImage";
+            this.selectedImage.Size = new System.Drawing.Size(633, 433);
+            this.selectedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.selectedImage.TabIndex = 0;
+            this.selectedImage.TabStop = false;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(763, 253);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 1;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Location = new System.Drawing.Point(12, 253);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrevious.TabIndex = 2;
+            this.buttonPrevious.Text = "Previous";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.buttonPrevious_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(661, 503);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(453, 503);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 4;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(557, 503);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 5;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(349, 503);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 6;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelCount.Location = new System.Drawing.Point(388, 13);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(26, 16);
+            this.labelCount.TabIndex = 7;
+            this.labelCount.Text = "0/0";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(850, 550);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonPrevious);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.selectedImage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Galary";
+            ((System.ComponentModel.ISupportInitialize)(this.selectedImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox selectedImage;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- galary/Form1.cs
+++ galary/Form1.cs
@@ -1,20 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
+using System.Drawing.Imaging;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+
 
 namespace galary
 {
     public partial class Form1 : Form
     {
+        List<string> lists = new List<string>();
+     
+        int imageNamber = 0;
+        int total = File.ReadAllLines("info.txt").Length;
+    
+
+
         public Form1()
         {
             InitializeComponent();
+            foreach (string str in File.ReadAllLines("info.txt"))
+            {
+                lists.Add(str);
+            }
+        }
+    
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+
+                dialog.Filter = "*.jpg|*.jpg|*.png|*.png";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+
+                    lists.Add(dialog.FileName);
+
+                }
+            }
+            selectedImage.Image = Image.FromFile(lists.Last());
+        }
+
+
+        //private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        //{
+        //    File.AppendAllLines("info.txt", lists);
+        //}
+        //this.FormClosing += Form2_FormClosing;
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+           
+            if (lists.Count != lists.Count) return;
+            {
+                imageNamber++;
+
+            }
+               if(imageNamber < total)
+                selectedImage.Image = Image.FromFile(lists[imageNamber]);
+         
+        }
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+         
+           // if (lists.Count == lists.Count)
+            {
+                imageNamber--;
+            }
+
+            if (imageNamber > 0) 
+               selectedImage.Image = Image.FromFile(lists[imageNamber]);
+          
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            File.AppendAllLines("info.txt", lists);
+            //foreach (string str in File.ReadAllLines("info.txt"))
+            //{
+            //    lists.Add(str);
+            //}
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+           
+        }
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
